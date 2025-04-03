58fa68bf9c39cf460a316d0bc2523efae1b1f297
webgipsy <babybird1801@gmail.com>
2025-04-03 16:48:55 +0630

    setup for bootstrap

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import { progressBar } from './src/views/partials/progressbar.js';
 import { PORT, HOST } from './src/config/config.js';
 import { connectDatabase } from './src/database/index.js';
 import expressEjsLayouts from 'express-ejs-layouts';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url); // Get the file path
+const __dirname = dirname(__filename); // Get the directory path
+
+console.log('Filename:', __filename);
+console.log('Directory:', __dirname);
 
 //db connection
 connectDatabase();
@@ -11,6 +20,9 @@ connectDatabase();
 const app = express();
 
 app.use(express.static('public'));
+
+// Serve static files
+app.use('/static', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 // Templating Engine
 app.use(expressEjsLayouts);
