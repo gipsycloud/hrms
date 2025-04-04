79395df4be71a13feeec473f00ce3a0541b3b4df
webgipsy <babybird1801@gmail.com>
2025-04-04 14:37:27 +0630

    setup for route

--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -1,33 +1,11 @@
 import express from "express";
 import { registerController } from "../controllers/web/authController.js";
+import { about, home } from "../controllers/web/webController.js";
 
 const route = express.Router();
 
-route.get('/register', registerController)
-
-route.get("/", (req, res) => {
-  const locals = {
-    title: "Home",
-    description: "Welcome to the home page",
-    keywords: "home, welcome",
-  }
-  res.render('index', { locals });
-});
-
-route.get("/about", (req, res) => {
-  res.render('about');
-});
-
-route.get("/signin", (req, res) => {
-  res.render('auth/login');
-});
-
-route.get(/(.*)/, (req, res, next) => {
-  res.status(StatusCode.NOT_FOUND).render('404', {
-    title: "404 Not Found",
-    description: "The page you are looking for does not exist.",
-    keywords: "404, not found",
-  });
-});
+route.get('/', home);
+route.get('/about', about);
+route.get('/register', registerController);
 
 export default route;
